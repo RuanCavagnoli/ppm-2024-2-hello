4f501de387e7fa5d01106b4c47933ca7fb34d810
RuanCavagnoli <ruan_cavagnoli@hotmail.com>
2024-08-19 20:13:56 -0300

    mudando o js

--- hello.js
+++ hello.js
@@ -1,7 +1,17 @@
 function helloWorld(){
     const input = document.querySelector('.texto');
+    const section = document.querySelector('section')
     if(input.value){
-        const p = document.querySelector('#paragrafo');
+        const p = document.createElement('p');
         p.innerHTML = `bem vindo a programação móvel ${input.value}`;
+        section.appendChild(p);
+        limpaInput();
     }
 }
+
+
+function limpaInput(){
+    const input = document.querySelector('.texto');
+    input.value = '';
+}
+
